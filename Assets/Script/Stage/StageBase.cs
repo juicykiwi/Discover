f1ab77dd963a1bef0697417b8771b54f28456a2d
Juicy <juicy@Juicyui-MacBook-Pro.local>
2018-05-07 22:37:52 +0900

    Stage 추가 Stage Document 추가

---
+++ Assets/Script/Stage/StageBase.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Discover.Stage.Field;
+
+namespace Discover.Stage
+{
+    public class StageBase : MonoBehaviour
+    {
+        protected List<FieldBase> _fieldList = new List<FieldBase>();
+    }
+}
